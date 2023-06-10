1f39e9193fb8d2b493b9802b7ca15c56630ac5e7
Nikhil Mittal <nmittal1_be22@thapar.edu>
2023-06-10 21:52:43 +0530

    Added some basic elements using Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-  return <></>;
+import { Typography ,TextField } from '@mui/material';
+import Button from '@mui/material/Button';
+
+
+export default function App() {
+  return <div>
+    <Typography variant="h2"
+    align="center">Welcome <br/>to <br/>30 Days of Code 🔥</Typography>
+  <Button variant="contained">Leader Board</Button>
+  <TextField label="Github ID" color="primary"></TextField>
+  <TextField label="Github Repo Link" color="primary"></TextField>
+</div>;
 }
 
-export default App;
+
