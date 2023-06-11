10f4da049935823470d2901dc4301d581db4b0a9
Nikhil Mittal <nmittal1_be22@thapar.edu>
2023-06-11 13:13:04 +0530

    Center aligned elements and added a know more section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import { Typography ,TextField } from '@mui/material';
+import { Typography, TextField, Box } from '@mui/material';
 import Button from '@mui/material/Button';
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
+import Detail from './Detail';
+
 
 
 export default function App() {
-  return <div>
-    <Typography variant="h2"
-    align="center">Welcome <br/>to <br/>30 Days of Code 🔥</Typography>
-  <Button variant="contained">Leader Board</Button>
-  <TextField label="Github ID" color="primary"></TextField>
-  <TextField label="Github Repo Link" color="primary"></TextField>
-</div>;
+  return <div className="page">
+    <Box textAlign="center"><Typography variant="h2" >Welcome <br />to <br />30 Days of Code 🔥</Typography><br />
+    <Button variant="contained">Leader Board</Button><br /><br />
+      <Typography textAlign="center">Enter your GitHub ID</Typography><br />
+      <TextField className="id-textfield" label="Github ID" color="primary"></TextField><br /><br />
+
+      <Typography textAlign="center" >Enter your GitHub Repository Link</Typography><br />
+      <TextField label="Github Repo Link" color="primary" ></TextField><br /><br />
+
+      <Button variant="contained" >Submit</Button><br /><br />
+
+      <Typography textAlign="center" >Know More</Typography><br />
+      <KeyboardDoubleArrowDownIcon />
+    </Box>
+    <Detail />
+  </div>;
 }
 
 
