b42ed6a72a6a8fdbbb37ee47e66c9aee78fdfbd8
Krishna Vig <114269519+vigkrishna@users.noreply.github.com>
2023-06-28 00:09:00 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,19 @@
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home/home";
+import LeaderboardPage from "./leaderboard/leaderboard";
+import Detail from './Home/Detail'
+export default function App() {
+  return(
+  <Router>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/leaderboard" element={<LeaderboardPage />} />
+    <Route path="/detail" element={<Detail />} />
+    </Routes>
+  </Router>
+  );
+}
+
+
+
